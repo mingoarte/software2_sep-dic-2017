af4168118704a8352e23e896ae4bedbc59400ecd
David Atias <davidalejandro23@gmail.com>
2017-10-17 23:26:09 -0400

    Modelo para clave pública - clave privada

--- servecaptcha/models.py
+++ servecaptcha/models.py
@@ -1,10 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import random
 from .utils import random_string
 
+class KeyPair(models.Model):
+    public_key = models.CharField(primary_key=True, max_length=64, default=random_string.alphanumeric)
+    private_key = models.CharField(max_length=64, default=random_string.alphanumeric, unique=True)
+
 class GeneratedCaptcha(models.Model):
-    id = models.AutoField(primary_key=True, default=random_string.alphanumeric)
-    client_key = models.CharField(max_length=64)
+    captcha_id = models.CharField(max_length=64, unique=True, default=random_string.alphanumeric)
+    keypair = models.ForeignKey(KeyPair)
     answer = models.CharField(max_length=30)
-    valid = models.BooleanField(default=True)
