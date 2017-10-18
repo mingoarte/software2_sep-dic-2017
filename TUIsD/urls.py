741a6a54be7f832c77d7c071b8fc69511a8524fb
mingoarte <domingoarte2850@gmail.com>
2017-10-17 21:55:29 -0400

    Configuracion del proyecto. Agregando sidebar a vista del constructor.

--- TUIsD/urls.py
+++ TUIsD/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url , include
 from django.contrib import admin
+from django.conf.urls import url
+from builder.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^builder/', include('builder.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
