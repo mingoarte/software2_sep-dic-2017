1f1164988b1e584a364c98fbd5bfd7e39efe2b2f
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-10-17 22:41:18 -0400

    se agregan comentarios para explicar la lógica de los views

--- servecaptcha/views.py
+++ servecaptcha/views.py
@@ -5,19 +5,24 @@ from captcha.image import ImageCaptcha
 import random
 
 def serveCaptchaImage(request, name):
-    
     image = ImageCaptcha()
-    #data = image.generate('%s' % name)
     image.write('%s' % name, 'static/%s.png' % name)
 
+    # Descomentar estas dos líneas hace que al ir a localhost/servecaptcha/image, 
+    # se muestre una imagen en el navegador con el captcha generado
+    # Hay que proporcionarle un valor como primer argumento a image.write para que 
+    # cree la imagen
+
+    #data = image.generate('%s' % name)
     #response = HttpResponse(data, content_type="image/png")
-    #return response
+
 
 def serveCaptchaAudio(request, name):
-    
     audio = CaptchaAuditivo()
-    #data = audio.generate()
     audio.write('%s' % name, 'static/%s.mp3' % name)
+
+    # Misma acotación de arriba
+
     #return render(request, 'servecaptcha/index.html')
 
 def serveCaptcha(request):
