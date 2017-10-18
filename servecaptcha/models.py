e77065af98ff8d23af8a635dc9098f737fb12782
David Atias <davidalejandro23@gmail.com>
2017-10-17 23:26:09 -0400

    Modelo para clave pública - clave privada

--- servecaptcha/models.py
+++ servecaptcha/models.py
@@ -5,6 +5,10 @@ from .utils import random_string
 
 class GeneratedCaptcha(models.Model):
     id = models.AutoField(primary_key=True, default=random_string.alphanumeric)
-    client_key = models.CharField(max_length=64)
+    keypair = models.ForeignKey(KeyPair)
     answer = models.CharField(max_length=30)
     valid = models.BooleanField(default=True)
+
+class KeyPair(models.Model):
+    public_key = models.CharField(primary_key=True, max_length=64, default=random_string.alphanumeric)
+    private_key = models.CharField(max_length=64, default=random_string.alphanumeric)
