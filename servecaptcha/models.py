ed34aa9d0382441231ea3aa8bc43fabcb99b2e1d
David Atias <davidalejandro23@gmail.com>
2017-10-17 22:36:38 -0400

    Modelo para captchas generados. Contiene id del captcha, respuesta, llave del cliente que lo generó, y si es válido

--- servecaptcha/models.py
+++ servecaptcha/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from .utils import random_string
 
-# Create your models here.
+class GeneratedCaptcha(models.Model):
+    id = models.AutoField(primary_key=True, default=random_string.alphanumeric)
+    client_key = models.CharField(max_length=64)
+    answer = models.CharField(max_length=30)
+    valid = models.BooleanField(default=True)
