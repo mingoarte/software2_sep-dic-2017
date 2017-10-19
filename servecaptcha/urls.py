9ba55944df083193c47ddce6340631cfe9247da9
David Atias <davidalejandro23@gmail.com>
2017-10-19 00:42:42 -0400

    Crear migración para crear llave publica-privada de prueba para TUISD
    - Quité migrations del .gitignore (lo había puesto inicialmente, pero no
    deberia estar ahi). Ver:
    https://stackoverflow.com/questions/28035119/should-i-be-adding-the-django-migration-files-in-the-gitignore-file
    
    - Agregué una migración que crea un keypair: (demoPublicKey,
    demoPrivateKey) para usarlo en TUISD, así cuando diseñas tu UI ves el captcha bien,
    y cuando generas el html se le pondrá el public key generado

--- servecaptcha/urls.py
+++ servecaptcha/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^image/(?P<captcha_id>[a-zA-Z0-9]{64})?$', views.serve_captcha_image),
     url(r'^audio/(?P<captcha_id>[a-zA-Z0-9]{64})?$', views.serve_captcha_audio),
     url(r'^generate_apikey/$', views.generate_apikey, name="generate_apikey"),
-    url(r'^generate_captcha/(?P<public_key>[a-zA-Z0-9]{64})/$', views.generate_captcha, name="generate_captcha"),
+    url(r'^generate_captcha/(?P<public_key>[a-zA-Z0-9]{1,64})/$', views.generate_captcha, name="generate_captcha"),
     url(r'^validate_captcha/$', views.validate_captcha, name="validate_captcha"),
     url(r'^$', views.api_documentation),
 
