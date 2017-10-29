07d77e5934f0541169b31e4d5a1d637f0e593d6c
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-10-29 10:59:17 -0400

    se agrega el controlador para mostrar el form builder

--- formBuilder/views.py
+++ formBuilder/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed, Http404, JsonResponse
 
-# Create your views here.
+
+def index(request):
+	return render(request, 'formBuilder/index.html')
