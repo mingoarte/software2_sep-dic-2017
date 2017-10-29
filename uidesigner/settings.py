e3376a0df205bab142eae190aae9c5ad5d48f9c5
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-10-29 10:29:00 -0400

    se agrega el path para los templates de la app formBuilder

--- uidesigner/settings.py
+++ uidesigner/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'uidesigner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['servecaptcha/templates'],
+        'DIRS': ['servecaptcha/templates', 'formBuilder/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
