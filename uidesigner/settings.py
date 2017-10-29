c794643abd446ba2bd628b6f0c75c979aa85d281
Julio Fuenmayor <jfuenmayor96@gmail.com>
2017-10-29 10:57:12 -0400

    se agrega la aplicacion de formbuilder al proyecto

--- uidesigner/settings.py
+++ uidesigner/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'servecaptcha.apps.ServecaptchaConfig',
-    'tuisd'
+    'tuisd',
+	'formBuilder'
 ]
 
 MIDDLEWARE = [
