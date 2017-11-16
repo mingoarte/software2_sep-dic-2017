e2406d88a4db2b6441cece6cd6a1fb5350eeed7a
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2017-11-15 23:27:28 -0400

    Añadido el captcha como patrón según el nuevo esquema.

--- captcha_pattern/models.py
+++ captcha_pattern/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-from builder.models import TemplateComponent
+from builder.models import Pattern
 
-class Captcha(TemplateComponent):
+class Captcha(Pattern):
     name = 'captcha'
+
+    def render(self):
+	    return render_to_string('patrones/captcha_pattern/build.html', {"pattern": self})
+
+    def __str__(self):
+	    return "Captcha"
