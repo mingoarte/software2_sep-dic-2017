689c6c5943cf09af69032bde6e85f294b9899c4b
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2017-11-20 12:29:53 -0400

    Añadidas las migraciones locales.

--- formBuilder/models.py
+++ formBuilder/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-from builder.models import TemplateComponent
+from builder.models import Pattern
 
-class Formulario(TemplateComponent):
+class Formulario(Pattern):
     name = 'formulario'
 
     form_json = JSONField(default=dict)
