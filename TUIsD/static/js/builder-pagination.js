1ad672fd8b64c5054a7721a642225355ff778e5a
Jose Herradez Hernandez <Jose Herradez Hernandez>
2017-12-07 08:20:21 -0400

    Change so it could load the pagination

--- TUIsD/static/js/builder-pagination.js
+++ TUIsD/static/js/builder-pagination.js
@@ -1,9 +1,10 @@
 // Envia informacion de creacion de patron
 function sendPaginationData() {
+  console.log("pagination data");
   // Locales con las opciones que quiero tomar del html del modal
-  var title = $('#modal-configuracion input[name="title"]').val()
-  var nItemsOnPage = $('#modal-configuracion input[name="nItemsOnPage"]').val()
-  var content = $('#modal-configuracion select[name="content"]').val()
+  var title = $('#modal-configuracion input[name="title"]').val();
+  var nItemsOnPage = $('#modal-configuracion input[name="nItemsOnPage"]').val();
+  var content = $('#modal-configuracion select[name="content"]').val();
   
   // Json con los datos y el url al que debe llamar
   return {
