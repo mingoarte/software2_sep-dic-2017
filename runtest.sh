656427b784fbd53958ee5fe11c0d7e6ba4546749
Alfredo Fanghella <alfredojfv.94@gmail.com>
2017-12-07 00:42:45 -0400

    Añadido script para ejecutar pruebas

---
+++ runtest.sh
@@ -0,0 +1,8 @@
+python3 -m venv env
+. env/bin/activate
+pip install selenium
+wget https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz
+tar -xvzf geckodriver*
+chmod +x geckodriver
+export PATH=$PATH:.
+python pruebas-python -m unittest pruebas-selenium/captcha/captcha.py
